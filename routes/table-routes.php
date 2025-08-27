9bd7d9611759522059d2e87d78d0c48f261c3d1c
Karthick <karthickk1996@hotmail.com>
2025-08-27 23:10:08 +0530

    Add tenancy middleware to table routes

--- routes/table-routes.php
+++ routes/table-routes.php
@@ -4,6 +4,7 @@ use Illuminate\Routing\Middleware\ValidateSignature;
 use Illuminate\Support\Facades\Route;
 use Modules\Table\Http\ActionController;
 use Modules\Table\Http\ExportController;
+use Stancl\Tenancy\Middleware\InitializeTenancyByDomain;
 
 Route::name('inertia-tables.')->prefix('/_inertia-tables/{table}/{name}')->group(function () {
     Route::post('/action/{action}/{state?}', ActionController::class)
@@ -17,4 +18,4 @@ Route::name('inertia-tables.')->prefix('/_inertia-tables/{table}/{name}')->group
     Route::post('/async-export/{export}/{state?}', ExportController::class)
         ->middleware(ValidateSignature::absolute('keys'))
         ->name('async-export');
-});
+})->middleware(['universal', InitializeTenancyByDomain::class]);
